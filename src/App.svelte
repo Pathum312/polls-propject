360f383f74394c21cd8a3e6bb567412a44b1d3c5
Pathum312 <pathum.2019557@iit.ac.lk>
2023-12-14 17:07:19 +0530

    Resuable Tab Component

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,25 @@
 <script>
     import Header from './components/Header.svelte';
     import Footer from './components/Footer.svelte';
+    import Tabs from './shared/Tabs.svelte';
+
+    // Tabs Management
+    let tabs = ['Current Polls', 'Add New Poll'];
+    let activeTab = 'Current Polls';
+
+    const tabChange = (tab) => {
+        activeTab = tab.detail;
+    };
 </script>
 
 <Header />
 <main>
-    <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-    </p>
+    <Tabs {tabs} {activeTab} on:tabChange={tabChange} />
+    {#if activeTab === 'Current Polls'}
+        <p>Poll List</p>
+    {:else if activeTab === 'Add New Poll'}
+        <p>Poll Add Form</p>
+    {/if}
 </main>
 <Footer />
 
