83df673251629f31ece1d06e2c4cdf028d40b44b
Pathum312 <pathum.2019557@iit.ac.lk>
2023-12-27 22:31:13 +0530

    LifeCycle Hooks

--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -1,15 +1,27 @@
 <script>
     import Poll from './Poll.svelte';
     import pollStore from '../stores/PollStores';
+    import { onMount, onDestroy } from 'svelte';
     export let polls = [];
 
-    pollStore.subscribe((data) => {
+    const unsubscribe = pollStore.subscribe((data) => {
         polls = data;
+    });
+
+    // This function runs when the pollList component is compiled/mounted
+    onMount(() => {
+        console.log('Mounted');
+    });
+
+    // This function runs when we switch to a different component other than the pollList
+    onDestroy(() => {
+        console.log('Destroyed');
+        unsubscribe();
     });
 </script>
 
 <div class="poll-list">
-    {#each polls as poll (poll.id)}
+    {#each $pollStore as poll (poll.id)}
         <Poll {poll} on:vote />
     {/each}
 </div>
